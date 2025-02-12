d1c69641e3388d6121796d49b45efba5d81e9f34
Michael Vella <michael.vella0110@gmail.com>
2025-02-12 19:12:19 +0100

    Implemented multiple try functionality
    Need to add some more input validation as well as not to allow users to enter the same numbers.

--- main.go
+++ main.go
@@ -1,33 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand/v2"
 	"os"
 )
 
-const lives = 1
+const lives = 3
 
 func main() {
 	var g int
+	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Generating a random number between 0 and 10...")
 
 	n := rand.IntN(10)
 
-	fmt.Printf("You have %d life to guess the random number generated...\n", lives)
+	fmt.Printf("You have %d lives to guess the random number generated...\n", lives)
 
-	fmt.Print("Enter your guess: ")
-	_, err := fmt.Scan(&g)
+	for i := 0; i < lives; i++ {
+		cl := i + 1
 
-	if err != nil {
-		fmt.Println("Invalid input! Please enter a valid integer.")
-		os.Exit(1)
+		fmt.Printf("\nYou are on life %d out of %d...\n", cl, lives)
+		fmt.Print("Enter your guess: ")
+
+		_, err := fmt.Scanln(&g)
+
+		if err != nil {
+			fmt.Println("Invalid input! No lives will be lost but please enter a valid integer...")
+
+			// clear input buffer
+			reader.ReadString('\n')
+
+			i--
+			continue
+		}
+
+		if n == g {
+			fmt.Printf("Correct guess. You win! You guessed %d and the random generated number was %d!\n", g, n)
+			break
+		} else {
+			fmt.Println("Incorrect guess...")
+		}
+
+		if lives == cl {
+			fmt.Println("WOMP WOMP. GAME OVER! Better luck next time.")
+		}
 	}
 
-	if n == g {
-		fmt.Printf("Correct guess! You guessed %d and the random generated number was %d!", g, n)
-	} else {
-		fmt.Printf("Incorrect guess. You guessed %d but the generated random number was %d...", g, n)
-	}
+	fmt.Println("\nThank you for playing! Hope to see you again soon...")
+	os.Exit(0)
 }
